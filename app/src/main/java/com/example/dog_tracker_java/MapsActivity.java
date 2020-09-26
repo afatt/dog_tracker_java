437a65a1a677cc7b5c294a138604405b905ec548
shen1 <shen1@shen1.com>
2020-09-26 19:00:26 -0400

    [bluetooth_serial_feature]
    Adds ConnectThread class

--- app/src/main/java/com/example/dog_tracker_java/MapsActivity.java
+++ app/src/main/java/com/example/dog_tracker_java/MapsActivity.java
@@ -2,6 +2,12 @@ package com.example.dog_tracker_java;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,6 +16,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,5 +52,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(initialPos).title("Doggo"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(initialPos));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(initialPos, 15.0f));
+    }
+
+    // Initializes Bluetooth adapter.
+    final BluetoothManager bluetoothManager =
+            (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+    BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+    // Ensures Bluetooth is available on the device and it is enabled. If not,
+    // displays a dialog requesting user permission to enable Bluetooth.
+    if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+}
+
+public class ConnectThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    public ConnectThread(BluetoothDevice device) {
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+        try {
+            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) { }
+        mmSocket = tmp;
+    }
+    public void run() {
+        mBluetoothAdapter.cancelDiscovery();
+        try {
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            try {
+                mSocket.close();
+            } catch (IOException closeException) { }
+            return;
+        }
+    }
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
     }
 }
